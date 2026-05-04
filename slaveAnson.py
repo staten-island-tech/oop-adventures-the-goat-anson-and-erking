9f2facf0ea39e1086c62501766716a0fd9390ffe
yes <ansonc43@nycstudents.net>
2026-05-04 10:22:29 -0400

    Update slaveAnson.py

---
+++ slaveAnson.py
@@ -0,0 +1,10 @@
+#IDEA : Sols RNG
+import random
+current_luck = 0
+aura_point = 0
+def RNG():
+    if aura_point <= 100:
+        print("Low Aura")
+        current_luck = 1
+    if aura_point
+RNG()
