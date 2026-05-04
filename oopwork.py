d10ec78f7d48031159400a7da46bcb5ead8e4341
ErkinA25 <erkina2@nycstudents.net>
2026-05-04 10:22:21 -0400

    Update oopwork.py

--- oopwork.py
+++ oopwork.py
@@ -1,1 +1,24 @@
-hi goat
+#sols rng
+import random
+random.randint
+aura_list = [
+    {
+        "name" : "Common aura",
+        "chance" : 0.50
+    }
+    ,
+    {
+        "name" : "Uncommon Aura",
+        "chance" : 0.25
+    }
+    ,
+    {
+        "name" : "Rare Aura",
+        "price" : 0.125
+    }
+    ,
+    {
+        "name" : "Rare Aura",
+        "price" : 0.125
+    }
+]
