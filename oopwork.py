aa19edbd090f765a70b8459f11855d69e51f3ce9
ErkinA25 <erkina2@nycstudents.net>
2026-05-06 10:17:19 -0400

    Update oopwork.py

--- oopwork.py
+++ oopwork.py
@@ -1,24 +1,81 @@
 #sols rng
 import random
 random.randint
+
 aura_list = [
     {
         "name" : "Common aura",
-        "chance" : 0.50
+        "chance" : 0.50,
+        aura_point += 10
     }
     ,
     {
         "name" : "Uncommon Aura",
-        "chance" : 0.25
+        "chance" : 0.25,
+        aura_point += 30
     }
     ,
     {
         "name" : "Rare Aura",
-        "price" : 0.125
+        "chance" : 0.125,
+        aura_point += 75
     }
     ,
     {
-        "name" : "Rare Aura",
-        "price" : 0.125
+        "name" : "Legendary Aura",
+        "chance" : 0.0625,
+        aura_point += 200
+    }
+    ,
+    {
+        "name" : "Mythic Aura",
+        "chance" : 0.03125,
+        aura_point += 450
+    }   
+    ,
+    {
+        "name" : "sub 5 whalen",
+        "chance" : 0.015625,
+        aura += 1000
+
+    }
+]
+
+medium_aura_list = [
+    {
+        "name" : "sub 5 whalen",
+        "chance" : 0.50,
+        aura_point += 1000
+    }
+    ,
+    {
+        "name" : "Holy Aura",
+        "chance" : 0.25,
+        aura_point += 1200
+    }
+    ,
+    {
+        "name" : "Lightning Aura",
+        "chance" : 0.125,
+        aura_point += 1750
+    }
+    ,
+    {
+        "name" : "Eclipse Aura",
+        "chance" : 0.0625,
+        aura_point += 2500
+    }
+    ,
+    {
+        "name" : "Void Aura",
+        "chance" : 0.03125,
+        aura_point += 3500
+    }   
+    ,
+    {
+        "name" : "Celestial Aura",
+        "chance" : 0.015625,
+        aura += 1000
+
     }
 ]
