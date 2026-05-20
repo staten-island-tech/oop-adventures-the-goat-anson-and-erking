a13d20511af4d38ecae7e60f8b57ff12a322105e
yes <ansonc43@nycstudents.net>
2026-05-20 10:13:55 -0400

    euwFCJOIAK

--- slaveAnson.py
+++ slaveAnson.py
@@ -10,28 +10,25 @@ root = tkinter.Tk()
 
 selected_list = "undefined"
 aura_level = 0
-aura_point = 10000000000000000000000
+aura_point = 1000
 def Titles():
     if aura_point >= 100000000000:
         print("Mogger High Tier Whalen Aura Points")
         selected_list = "WHALEN"
     
-    elif aura_point >= 1000000000:
+    elif aura_point >= 100000000000:
         print("Extrodinary Aura Points") 
         selected_list = "extraordinary"    
-    
- 
-    
-    elif aura_point >= 10000000:
+    elif aura_point >= 10000000000:
         print("High Aura Points")
         selected_list = "high"
     
-    elif aura_point >= 10000:
+    elif aura_point >= 10000000:
         print("Medium Aura Points")
         selected_list = "medium"
     
     
-    elif aura_point >= 1000:
+    elif aura_point >= 100000:
         print("Low Aura Points")
         selected_list = "low"
 
@@ -59,6 +56,7 @@ def Titles():
 
     # Roll aura
     result = random.choices(names, weights=weights, k=1)[0]
+    aura_point+=result["aura_quantity"]
 
     print("You rolled:", result)
 
@@ -72,24 +70,19 @@ Titles()
 
 
 
+import tkinter as tk
 
+root = tk.Tk()
 
+def button_pressed():
+    Titles()
 
+button = tk.Button(
+    root,
+    text="Click Me",
+    command=button_pressed
+)
 
-
-
-
-root.geometry("500x500")
-roll_button = tkinter.Button(root, text = "Roll for Auras")
-roll_button.pack(side = "bottom", ipady=20, fill="x")
+button.pack()
 
 root.mainloop()
-with open("oopwork.json", "r") as file:
-    data = json.load(file)
-
-x = True
-while x == True:
-    if roll_button == True:
-        Titles()
-    else:
-        break
