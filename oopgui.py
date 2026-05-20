35672a5a6ca257286cedc9d70c71c4ef957b79e4
ErkinA25 <erkina2@nycstudents.net>
2026-05-20 09:40:36 -0400

    Update oopgui.py

--- oopgui.py
+++ oopgui.py
@@ -3,6 +3,6 @@ root = tkinter.Tk()
 
 root.geometry("500x500")
 roll_button = tkinter.Button(root, text = "Roll for Auras")
-roll_button.pack(side = "bottom", pady=20, padx=90)
+roll_button.pack(side = "bottom", ipady=20, fill="x")
 
 root.mainloop()
